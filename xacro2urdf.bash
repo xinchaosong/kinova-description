b0a1cf119642537c9097a08b0e5c8733973a75a9
Xinchao Song <git@xinchaosong.com>
2021-06-03 19:10:24 -0400

    Add all files

---
+++ xacro2urdf.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+xacro xacro/j2n4s300_standalone.xacro > urdf/j2n4s300_standalone.urdf
+xacro xacro/j2n6s200_standalone.xacro > urdf/j2n6s200_standalone.urdf
+xacro xacro/j2n6s300_standalone.xacro > urdf/j2n6s300_standalone.urdf
+xacro xacro/j2n7s300_standalone.xacro > urdf/j2n7s300_standalone.urdf
+xacro xacro/j2s6s200_standalone.xacro > urdf/j2s6s200_standalone.urdf
+xacro xacro/j2s6s300_standalone.xacro > urdf/j2s6s300_standalone.urdf
+xacro xacro/j2s7s300_standalone.xacro > urdf/j2s7s300_standalone.urdf
+xacro xacro/m1n4s200_standalone.xacro > urdf/m1n4s200_standalone.urdf
+xacro xacro/m1n6s200_standalone.xacro > urdf/m1n6s200_standalone.urdf
+xacro xacro/m1n6s300_standalone.xacro > urdf/m1n6s300_standalone.urdf
+xacro xacro/two_arm_robot_example_standalone.xacro > urdf/two_arm_robot_example_standalone.urdf
